e9bb16424ec7c90f5201072551661ffc0f4bf53d
Garcia0404 <julioxenrique@gmail.com>
2024-04-29 05:14:48 -0500

    links

--- src/helper/proyects.ts
+++ src/helper/proyects.ts
@@ -11,7 +11,7 @@ export const proyects:Proyect[] = [
     img:'/musicPlayer_so.png',
     description:'¡Descubre el ritmo único de la banda estadounidense "The Strokes" con nuestro reproductor de música inspirado en Spotify!',
     tools:['/js.svg','/react.svg','/tailwind.svg'],
-    link:'https://music-player-kappa-orcin.vercel.app/',
+    link:'https://eshop-red.vercel.app/',
   },
   {
     name:'Eshop',
@@ -23,7 +23,7 @@ export const proyects:Proyect[] = [
   {
     name:'Rick and Morty API',
     img:'/RyM_so.png',
-    description:'Con una interfaz elegante y fácil de usar, encontrarás todo lo que necesitas para satisfacer tu curiosidad sobre este universo tan querido. Consumo de API de Rick and morty usando AXIOS.',
+    description:'Con una interfaz elegante y fácil de usar, encontrarás todo lo que necesitas para satisfacer tu curiosidad sobre este universo tan querido.',
     tools:['/js.svg','/react.svg','/tailwind.svg'],
     link:'https://r-mv2.vercel.app/1',
   },
@@ -32,7 +32,7 @@ export const proyects:Proyect[] = [
     img:'/todo_so.png',
     description:'Imagina un mundo donde cada tarea tiene su lugar, donde puedes priorizar tus responsabilidades con solo un par de toques en la pantalla. Con nuestra aplicación, ese mundo es una realidad.',
     tools:['/ts.svg','/react.svg','/tailwind.svg'],
-    link:'https://todo-bzlyokbrq-julios-projects-6144f1d2.vercel.app/',
+    link:'https://todo-nine-alpha-80.vercel.app/',
   },
 
 ]
