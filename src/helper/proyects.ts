4546e665df2023902dba4cd5f474b53da6ff62bb
Garcia0404 <julioxenrique@gmail.com>
2024-07-01 22:00:29 -0500

    yourBank proyect

--- src/helper/proyects.ts
+++ src/helper/proyects.ts
@@ -19,5 +19,12 @@ export const proyects:Proyect[] = [
     description:'Tienda virtual que ofrece una experiencia de compras en línea, destacado por su carrito de compras funcional.',
     tools:['/js.svg','/react.svg','/tailwind.svg'],
     link:'https://eshop-isild4k1j-julios-projects-6144f1d2.vercel.app/',
+  },
+  {
+    name:'YourBank',
+    img:'/yourBank_so.png',
+    description:'Página web moderna y responsiva basada en un diseño detallado creado en Figma. La página está optimizada para un rendimiento rápido y una experiencia de usuario fluida.',
+    tools:['/js.svg','/astro.svg','/preact.svg','/tailwind.svg'],
+    link:'https://your-bank-pink.vercel.app/',
   }
 ]
